3632a88958147c67c81d5a12cdc814f451c9da61
Ricardo Rolim de Oliveira <ricardorolim27@gmail.com>
2022-08-10 12:40:44 -0300

    Update Empresa.java

--- src/alura/com/gerenciador/entities/Empresa.java
+++ src/alura/com/gerenciador/entities/Empresa.java
@@ -6,6 +6,7 @@ public class Empresa {
 	
 	private Integer id;
 	private String 	name;
+	private String  cnpj;
 	private Date 	dataAbertura = new Date();
 	
 	public String getName() {
@@ -30,6 +31,14 @@ public class Empresa {
 	
 	public void setDataAbertura(Date dataAbertura) {
 		this.dataAbertura = dataAbertura;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
 	}
 	
 
