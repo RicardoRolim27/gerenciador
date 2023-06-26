046b0ebdbd05f7dab96d3586d021a98edce66c3c
Ricardo Rolim de Oliveira <ricardorolim27@gmail.com>
2023-06-26 10:58:11 -0300

    atualizacao 26/06/2023
    atualizacao 26/06/2023

--- src/alura/com/gerenciador/connections/ConnectionFactory.java
+++ src/alura/com/gerenciador/connections/ConnectionFactory.java
@@ -1,18 +1,33 @@
 package alura.com.gerenciador.connections;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		
+		ComboPooledDataSource pool =  new ComboPooledDataSource();
+		
+		pool.setJdbcUrl("jdbc:mysql://localhost/gerenciador?useTimezone=true&serverTimezone=UTC");
+		pool.setUser("ricardo_rolim");
+		pool.setPassword("Desenv@123");
+		
+		pool.setMaxPoolSize(15);
+		
+		this.dataSource = pool;
+		
+	}
+	
 	public Connection criaConexao() throws SQLException {
 		
-		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
-		
-		return DriverManager
-				.getConnection("jdbc:mysql://localhost/gerenciador?useTimezone=true&serverTimezone=UTC", 
-						"ricardo_rolim", "Desenv@123");
+		return this.dataSource.getConnection();
 	}
 
 }
